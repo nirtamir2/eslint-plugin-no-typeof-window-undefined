0fd22f556bcf32750dc6af43c07d583aafaefb55
Nir Tamir <nirtamir2@gmail.com>
2022-12-02 14:24:42 +0200

    commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { recommended } from "./config/recommended";
 import { noTypeofWindowUndefined } from "./rules/no-typeof-window-undefined";
 
-// eslint-disable-next-line import/no-anonymous-default-export,import/no-unused-modules
-export default {
+// eslint-disable-next-line unicorn/prefer-module,import/no-commonjs
+module.exports = {
   rules: {
     "no-typeof-window-undefined": noTypeofWindowUndefined,
   },
